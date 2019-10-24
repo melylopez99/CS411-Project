8b8240e7f5a541c3bb4b710273d6e1b8da4ced05
mlopez <mlopez99@bu.edu>
2019-10-24 15:48:20 -0400

    parses through to get places

--- test_foursquare.py
+++ test_foursquare.py
@@ -6,6 +6,8 @@ Created on Fri Oct 18 12:55:53 2019
 """
 
 import json, requests
+
+
 url = 'https://api.foursquare.com/v2/venues/explore'
 
 params = dict(
@@ -13,8 +15,21 @@ params = dict(
   client_secret='JSIL4ZZV0E4WMABHVV1AO5GWDAPUS5P0KQQNHK5JKDDIUNBV',
   v='20180323',
   ll='42.3601,-71.0589',
-  query='food',
-  limit=2
+  query='chicken',
+  limit=4
 )
 resp = requests.get(url=url, params=params)
 data = json.loads(resp.text)
+data = data['response']['groups'][0]['items']   
+
+
+def getPlaces(): 
+    places = {} 
+    for place in data: 
+        name = place['venue']['name']
+        location = ','.join(place['venue']['location']['formattedAddress'])
+        places[name] = location 
+    return places 
+
+
+places = getPlaces()         
